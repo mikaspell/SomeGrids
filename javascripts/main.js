136f49ea7b3660a106d6d5709a74c6c805ea49e5
Dima <work@melkee.ru>
2015-02-09 01:47:48 +0300

    no message

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+// console.log('This would be the main JS file.');
+$(function() {
+	var scrollAside = $('header').innerHeight();
+
+	$(window).scroll(function(){
+
+		if( $(this).scrollTop() >= scrollAside ) {
+
+			$('#sidebar').addClass('js-scrolledMenu');
+		} else {
+
+			$('#sidebar').removeClass('js-scrolledMenu');
+		}
+	})
+})
